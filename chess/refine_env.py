951f38e069dce57760b4da79f613f5f28819b0fa
deeplearning <deep@learning.com>
2018-12-17 01:16:35 +0900

    chessenv now has color selection

--- chess/refine_env.py
+++ chess/refine_env.py
@@ -3,13 +3,16 @@ import re
 
 class ChessEnvironment:
 
-	def __init__(self):
+	def __init__(self, white = True):
 		self.pos = sunfish.Position(sunfish.initial, 0, (True,True), (True,True), 0, 0)
 		self.searcher = sunfish.Searcher()
-		self.isPlayerTurn = True;
+		self.isPlayerTurn = white
+		self.isPlayerwhite = white
 
-	def reset(self):
+	def reset(self, white = True):
 		self.pos = sunfish.Position(sunfish.initial, 0, (True,True), (True,True), 0, 0)
+		self.isPlayerTurn = white
+		self.isPlayerwhite = white
 
 	def state(self):
 		return self.pos.board if self.isPlayerTurn else self.pos.rotate().board
@@ -22,11 +25,16 @@ class ChessEnvironment:
 		return False
 
 	def move_player(self, move):
+		if not self.isPlayerTurn:
+			return False
 		if self.game_end():
 			return False
 		match = re.match('([a-h][1-8])'*2, move)
 		if match:
-			move = sunfish.parse(match.group(1)), sunfish.parse(match.group(2))
+			if self.isPlayerwhite:
+				move = sunfish.parse(match.group(1)), sunfish.parse(match.group(2))
+			else:
+				move = 119 - sunfish.parse(match.group(1)), 119 - sunfish.parse(match.group(2))
 		else:
 			return False
 		if move not in self.pos.gen_moves():
@@ -39,5 +47,8 @@ class ChessEnvironment:
 		move, score = self.searcher.search(self.pos, secs=0.5)
 		self.pos = self.pos.move(move)
 		self.isPlayerTurn = True
-		return sunfish.render(119-move[0]) + sunfish.render(119-move[1])
+		if self.isPlayerwhite:
+			return sunfish.render(119-move[0]) + sunfish.render(119-move[1])
+		else:
+			return sunfish.render(move[0]) + sunfish.render(move[1])
 
