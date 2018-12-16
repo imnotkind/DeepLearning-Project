194fdc5a85b9122db925503ba83c3a0b968dd8ee
haebin307 <billy307@daum.net>
2018-12-17 04:26:36 +0900

    cleanclean

--- backend/static/main.js
+++ backend/static/main.js
@@ -1,6 +1,6 @@
 
 let cfg = {
     position: 'r1bqkbnr/pppp1ppp/2n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R',
-    pieceTheme: 'static/chessboardjs-0.3.0/img/chesspieces/alpha/{piece}.png'
+    pieceTheme: 'static/chessboardjs-0.3.0/img/chesspieces/wikipedia/{piece}.png'
 }
 let board = ChessBoard('board', cfg);
