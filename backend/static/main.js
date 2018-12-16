12798ca0f94c52966c775a49bf9b07cd46227183
haebin307 <billy307@daum.net>
2018-12-17 07:35:12 +0900

    zxvas

--- backend/static/main.js
+++ backend/static/main.js
@@ -1,6 +1,18 @@
 
-let cfg = {
-    position: 'r1bqkbnr/pppp1ppp/2n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R',
-    pieceTheme: 'static/chessboardjs-0.3.0/img/chesspieces/wikipedia/{piece}.png'
+
+
+let refresh = function(){
+    let cfg1 = {
+        position: 'r1bqkbnr/pppp1ppp/2n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R',
+        pieceTheme: 'static/chessboardjs-0.3.0/img/chesspieces/wikipedia/{piece}.png'
+    }
+    ChessBoard('board1', cfg1);
+
+    let cfg2 = {
+        position: 'r1bqkbnr/pppp1ppp/2n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R',
+        pieceTheme: 'static/chessboardjs-0.3.0/img/chesspieces/wikipedia/{piece}.png'
+    }
+    ChessBoard('board2', cfg2);
 }
-let board = ChessBoard('board', cfg);
+
+setInterval(refresh, 1000);
