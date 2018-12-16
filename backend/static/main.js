e431c9a5e811297e7c582f1eea2a520b2f5d2396
haebin307 <billy307@daum.net>
2018-12-17 07:35:32 +0900

    beq

--- backend/static/main.js
+++ backend/static/main.js
@@ -2,6 +2,8 @@
 
 
 let refresh = function(){
+    
+
     let cfg1 = {
         position: 'r1bqkbnr/pppp1ppp/2n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R',
         pieceTheme: 'static/chessboardjs-0.3.0/img/chesspieces/wikipedia/{piece}.png'
