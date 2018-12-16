665715568db806c563b3f4798057aeaa6b502b43
haebin307 <billy307@daum.net>
2018-12-17 04:23:30 +0900

    chesspiece resolution by cfg

--- backend/static/main.js
+++ backend/static/main.js
@@ -1,2 +1,6 @@
-let ruyLopez = 'r1bqkbnr/pppp1ppp/2n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R';
-let board = ChessBoard('board', ruyLopez);
+
+let cfg = {
+    position: 'r1bqkbnr/pppp1ppp/2n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R',
+    pieceTheme: 'static/chessboardjs-0.3.0/img/chesspieces/alpha/{piece}.png'
+}
+let board = ChessBoard('board', cfg);
